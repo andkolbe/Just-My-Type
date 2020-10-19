60b1605d26d8f01f5d22c199eed12089d57ab830
Andrew <kolbe1129@gmail.com>
2020-10-19 18:50:54 -0400

    Almost Ready! Needs some help at the very end

--- scripts.js
+++ scripts.js
@@ -1,122 +1,102 @@
-
-// Hide uppercase keyboard when page loads
-$('body').ready(function () {  // could also use $('body').ready()
-    $('#keyboard-upper-container').hide();
-});
-
-
-// When the shift key is held down, hide lowercase keyboard and show uppercase one
-$('body').keydown(function (e) {  
-   if(e.which === 16) {
-       $('#keyboard-lower-container').hide();
-       $('#keyboard-upper-container').show();
-   }
-});
-
-
-// When the shift key is released, hide uppercase keyboard and show uppercase one
-$('body').keyup(function (e) {  
-   if(e.which === 16) {
-       $('#keyboard-lower-container').show();
-       $('#keyboard-upper-container').hide();
-   }
-});
-
-
-// When keys are pressed, they should be highlighted in the browser
-$('body').keypress(function(e) {
-    //highlight pressed key
-    let key = $('#' + e.which);  // <= what?
-    $(key).css('background-color', 'yellow');
-
-    $('body').keyup(function(e) {
-      //unhighlight released key
-      $(key).css('background-color', '');
-    });
-  });
-
-
-/*
-The sentences in the provided array should be displayed at the top of the page one sentence at a time. 
-Once the sentence has been completed, the next in line should appear. 
-There is already a div with id="sentence" in your html file. 
-This is where you will display each sentence one at a time.
-*/
-
 let sentences = [
-    'ten ate neite ate nee enet ite ate inet ent eate', 
-    'Too ato too nOt enot one totA not anot tOO aNot', 
-    'oat itain oat tain nate eate tea anne inant nean', 
-    'itant eate anot eat nato inate eat anot tain eat', 
+    'ten ate neite ate nee enet ite ate inet ent eate',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tain nate eate tea anne inant nean',
+    'itant eate anot eat nato inate eat anot tain eat',
     'nee ene ate ite tent tiet ent ine ene ete ene ate'
 ];
 let sentenceIdx = 0;
 let letterIdx = 0;
-
-$('#sentence').text(sentences[sentenceIdx]);
-
-    
-
-//  Highlight the currently expected letter in the on-screen sentence that should be typed next
-    // move the #yellow-block along the letterIdx
-// if (letterIdx < sentences.length) {} ??
+let currentSentence = sentences[sentenceIdx];
+let currentLetter = currentSentence[letterIdx];
+let numberOfMistakes = 0;
+const startTime = new Date();
 
 
-//Also display the currently expected letter in the center (div id="target-letter" provided for you)
-$('#target-letter').css('margin', 'auto');
+// Hide uppercase keyboard when page loads
+//$('body').ready(function () {  <= Not necessary
+$('#keyboard-upper-container').hide();
+//});
+
+$('#sentence').text(currentSentence);
+$('#target-letter').text(currentLetter); //The letter on the screen
 
 
-
-
-
-/*
-//HINTS
-
-(split(' ').length)
-
-
-$('body').keydown(function (e) {  //keydown doesn't recognize upper and lowercase 
-    console.log(e.which)
-});
-$('body').keyup(function (e) {});
-
-//BULK OF GAME 
-$('body').keypress(function (e) {  //keypress does recognize upper and lowercase
-    console.log(e.which)
-});
-/don't nest or use any other key listeners
- 
-
-
-console.log('anteater'.charCodeAt(0)); 
-
-logs 97, the same thing as 
-
-$('body').keypress(function (e) {  
-    console.log(e.which)
+// When the shift key is held down, hide lowercase keyboard and show uppercase one
+$('body').keydown(function (e) {
+    if (e.which === 16) {
+        $('#keyboard-lower-container').hide();
+        $('#keyboard-upper-container').show();
+    }
 });
 
 
-const sentences = [
-    'The dog is named Odin',
-    'My dog is nicknamed Odie',
-    'A doggo is named Odin',
-    'There is a dog is named Odin',
-];
+// When the shift key is released, hide uppercase keyboard and show uppercase one
+$('body').keyup(function (e) {
+    $('.highlight').removeClass('highlight');
+    if (e.which === 16) {
+        $('#keyboard-lower-container').show();
+        $('#keyboard-upper-container').hide();
+    }
+});
 
-let sentenceIdx = 1;
-let letterIdx = 0;
 
-console.log(sentences[sentenceIdx][letterIdx]);
+// When keys are pressed, they should be highlighted in the browser
+$('body').keypress(function (e) {
+    //highlight pressed key
+    $('#' + e.which).addClass('highlight');
+    //moves yellow block
+    $('#yellow-block').css('margin-left', '+=17.5px');
 
-sentenceIdx++;
-letterIdx++;
-letterIdx++;
-letterIdx++;
-$('#letter').text(sentences[sentenceIdx][letterIdx]);
+    if (e.which === currentSentence.charCodeAt(letterIdx)) {
+        $('#feedback').append('<span class="glyphicon glyphicon-ok"></span>')
+    } else {
+        $('#feedback').append('<span class="glyphicon glyphicon-remove"></span>')
+        numberOfMistakes++;
+    }
+    //increment letter index and get next letter
+    letterIdx++;
+    currentLetter = currentSentence[letterIdx];
+    //update DOM with new letter
+    $('#target-letter').text(currentLetter);
 
-console.log(sentences[sentenceIdx][letterIdx]);
+    //interate through all letters in all sentences
+    if (letterIdx === currentSentence.length) {
+        sentenceIdx++
+        letterIdx = 0;
+        currentSentence = sentences[sentenceIdx];
+        currentLetter = currentSentence[letterIdx];
+        $('#sentence').text(currentSentence);
+        $('#target-letter').text(currentLetter);
+        $('#yellow-block').css('margin-left', '17.5px')
+        $('#feedback').empty();
+    // What to do when we get to the end
+    } if (sentenceIdx === sentences.length - 1) {
+        for (let i = 0; i < sentences.length; i++) {
+            numberOfWords = sentences[i].split(' ').length;
+        }
+        let endTime = new Date();
+        let minutes = (endTime - startTime) / 60000; // 1000ms = 1s  60s = 1min
+        //The user's words per minute should be calculated and displayed on the screen when you reach the end of the last sentence
+        const wordCount = Math.floor(numberOfWords / minutes - 2 * numberOfMistakes);
+        $('#sentence').text('End of Game! You had ' + wordCount + ' words per minute');
+        $('#feedback').empty();
+        //$('#yellow-block').empty(); Doesn't work!
+        //There should be a delay so the user can see the score. Then ask the user whether they would like to play again
+        setTimeout(function () {
+            $('#feedback').append('<button>Play Again!</button>')
+            $('button').css({
+                'background-color': 'red',
+                'color': 'white',
+                'font': '15px Arial, sans-serif',
+                'padding': '15px',
+                'border-color': 'white',
+            })
+            $('button').click(function () {
+                location.reload();
+            })
+        }, 2000);
 
-The last index is length - 1
-*/
+    }
+});
 
